5cd1f5e35956376c0c6ab6494f4548d72454d640
DesmondANIMUS <desmondmiles36@gmail.com>
2016-10-29 19:37:54 +0530

    Changes in models.py

--- acelibraryapp/models.py
+++ acelibraryapp/models.py
@@ -3,3 +3,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Problem(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+    approval_status =   models.BooleanField(default=False)
+    solution    =   models.TextField(
+            blank=True, null=True
+    )            
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def _str_(self):
+        return self.title
