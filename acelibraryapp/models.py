faac974d0f3e6e79d679e6c4972b167d82e72d17
Ashish <ashishpahwa7@gmail.com>
2016-10-30 02:57:46 +0530

    integrated front end

--- acelibraryapp/models.py
+++ acelibraryapp/models.py
@@ -1,21 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.db import timezones
+from django.utils import timezone
 
 # Create your models here.
 class Problem(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
-    approval_status =   models.BooleanField(default=False)
-    solution    =   models.TextField(
-            blank=True, null=True
-    )            
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    approval_status = models.BooleanField(default=False)
+    solution =  models.TextField()            
 
     def publish(self):
         self.published_date = timezone.now()
