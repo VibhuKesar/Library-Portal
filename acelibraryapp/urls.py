63d62f6e6cb4859cea5d776c0091002127512a4a
Ashish <ashishpahwa7@gmail.com>
2016-10-31 18:35:50 +0530

    added authentication

--- acelibraryapp/urls.py
+++ acelibraryapp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home_page'),
+    url(r'^$', views.index, name='index'),
+    url(r'^home/', views.home, name='home'),
+    url(r'^login/', views.member_login, name='member_login'),
 ]
--- acelibraryapp/views.py
+++ acelibraryapp/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 
-def home_page(request):
-	return render(request, 'acelibraryapp/home_page.html', {})
+def index(request):
+	return render(request, 'acelibraryapp/index.html', {})
+
+
+@login_required
+def home(request):
+
+	return render(request,'acelibraryapp/home.html',{})
+
+
+def member_login(request):
+
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return redirect('acelibraryapp.views.home')
+			else:
+				print("Disabled account")
+		else:
+			print("Invalid login")
+
+	return render(request, 'acelibraryapp/index.html', {})
